5aa42e106ca19dec89a31443b79172918b106bb6
Dylan Chilet <98715472+TCHDyLaN@users.noreply.github.com>
2025-05-26 23:40:32 -0500

    uso de setters en los atributos

--- src/Principal.java
+++ src/Principal.java
@@ -1,33 +1,43 @@
+import aluracursosScreenmatch.pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         pelicula miPelicula = new pelicula();
-        miPelicula.nombre_pelicula = "Encanto";
-        miPelicula.duracion = 110;
-        miPelicula.fecha_estreno= "10/06/2025";
-        miPelicula.disponible = false;
+        miPelicula.setnombre_pelicula("Encanto");
+        miPelicula.setduracion(110);
+        miPelicula.setfecha_estreno("10/06/2025");
+        miPelicula.setdisponible(false);
 
 
 
         miPelicula.mostrarFichaTecnica();
         miPelicula.evalua(7.8);
         miPelicula.evalua(8.5);
+        miPelicula.evalua(10.4);
 
         System.out.println(miPelicula.getsumaEvaluaciones());
-        System.out.println(miPelicula.calcularMedia());
+        System.out.println(miPelicula.getcalcularMedia());
         
 
 
 
-        pelicula otraPelicula = new pelicula();
-        otraPelicula.nombre_pelicula = "Matrix";
-        otraPelicula.fecha_estreno = "13/06/2025";
-        otraPelicula.duracion = 120;
-        otraPelicula.disponible= true;
+
+
+
+
+
+
+
+        // pelicula otraPelicula = new pelicula();
+        // otraPelicula.nombre_pelicula = "Matrix";
+        // otraPelicula.fecha_estreno = "13/06/2025";
+        // otraPelicula.duracion = 120;
+        // otraPelicula.disponible= true;
 
         //otraPelicula.mostrarFichaTecnica();
 
-        System.out.println();
 
-        System.out.println("Mi otra pelicula es: " + otraPelicula.nombre_pelicula + " y se estrenará el " + otraPelicula.fecha_estreno);
+
+        // System.out.println("Mi otra pelicula es: " + otraPelicula.nombre_pelicula + " y se estrenará el " + otraPelicula.fecha_estreno);
     }
 }
---
+++ src/aluracursosScreenmatch/pelicula.java
@@ -0,0 +1,51 @@
+package aluracursosScreenmatch;
+public class pelicula {
+    private  String nombre_pelicula;
+    private String fecha_estreno;
+    private int duracion;
+    private boolean disponible;
+    private  double sumaEvaluaciones;
+    private int TotalEvaluaciones;
+
+
+    public void setnombre_pelicula(String nombre_pelicula){
+        this.nombre_pelicula = nombre_pelicula;
+    }
+
+    public void setduracion(int duracion){
+        this.duracion = duracion;
+    }
+
+    public void setdisponible(boolean disponible){
+        this.disponible = disponible;
+    }
+
+    public void  setfecha_estreno(String fecha_estreno){
+        this.fecha_estreno = fecha_estreno;
+    }
+
+
+
+    public double  getsumaEvaluaciones(){
+        return  sumaEvaluaciones;
+    }
+    public int getTotalEvaluaciones(){
+        return TotalEvaluaciones;
+    }
+
+    public void mostrarFichaTecnica(){
+        System.out.println("El nombre de la pelicula es: " + nombre_pelicula + " y se estrenará el " + fecha_estreno + "y tiene una duración de " + duracion + " minutos.");
+        System.out.println("¿Está disponible? " + (disponible ? "Sí" : "No"));
+    }
+
+    public void evalua (double nota){
+
+        sumaEvaluaciones += nota;
+        TotalEvaluaciones++;
+    }
+
+    public double getcalcularMedia (){
+        return sumaEvaluaciones / TotalEvaluaciones;
+    }
+
+}
